7148742184438c86eaa0f7d4c43905234817bafe
TunaMonsieur <145128607+TunaMonsieur@users.noreply.github.com>
2025-10-30 02:19:17 +0700

    Update page

--- src/app/companyProfilePage/page.tsx
+++ src/app/companyProfilePage/page.tsx
@@ -1,4 +1,3 @@
-import Sidebar from '@/app/components/companyProfile/Sidebar';
 import Header from '@/app/components/companyProfile/Header';
 import CompanyHeader from '@/app/components/companyProfile/CompanyHeader';
 import CompanyInfo from '@/app/components/companyProfile/CompanyInfo';
@@ -9,25 +8,20 @@ import JobListings from '@/app/components/companyProfile/JobListings';
 
 export default function CompanyProfilePage() {
   return (
-    <div className="min-h-screen bg-gray-50">
-      <div className="flex max-w-7xl mx-auto">
-        <Sidebar />
-        <main className="flex-1">
-          <Header />
-          <div className="p-8">
-            <CompanyHeader />
-            <div className="grid grid-cols-3 gap-6">
-              <div className="col-span-2">
-                <CompanyInfo />
-                <Benefits />
-                <Contact />
-              </div>
-              <div className="col-span-1">
-                <JobListings />
-              </div>
-            </div>
+    <div className="min-h-screen">
+      <Header />
+      <div className="p-8">
+        <CompanyHeader />
+        <div className="grid grid-cols-3 gap-6">
+          <div className="col-span-2">
+            <CompanyInfo />
+            <Benefits />
+            <Contact />
           </div>
-        </main>
+          <div className="col-span-1">
+            <JobListings />
+          </div>
+        </div>
       </div>
     </div>
   );
